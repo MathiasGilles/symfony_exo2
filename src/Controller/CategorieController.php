d9c81c3b7914d289cc5b7da216d2f21ad8806c08
Mathias <mathiasgilles136@gmail.com>
2020-03-10 09:22:57 +0100

    crud categories done

--- src/Controller/CategorieController.php
+++ src/Controller/CategorieController.php
@@ -2,7 +2,10 @@
 
 namespace App\Controller;
 
+use App\Entity\Categorie;
+use App\Form\CategorieType;
 use App\Repository\CategorieRepository;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 
@@ -30,6 +33,61 @@ class CategorieController extends AbstractController
 
         return $this->render('categorie/_categorie_table.html.twig', [
             'categories' => $categories,
+        ]);
+    }
+
+    /**
+     * @Route("/categorie/new",name="categorie_new")
+     * @Route("/categorie/edit/{id}",name="categorie_edit")
+     */
+    public function new(Categorie $categorie = null,Request $request)
+    {
+         if ($categorie == null) {
+            $categorie = new Categorie();
+        }
+        $manager = $this->getDoctrine()->getManager();
+        $form = $this->createForm(CategorieType::class, $categorie);
+        $form->handleRequest($request);
+        if ($form->isSubmitted() && $form->isValid()) {
+            $manager->persist($categorie);
+            $manager->flush();
+            $this->addFlash("success","Categorie enregistrée");
+        }
+        
+       return $this->render('categorie/categorie_new.html.twig',[
+           'formCategorie' => $form->createView(),
+           'editTitle' => $categorie->getId() != null,
+            'editMode' => $categorie->getId() != null,
+       ]); 
+    }
+
+    /**
+     * @Route("/categorie/delete/{id}",name="categorie_delete")
+     */
+    public function delete(Categorie $categorie = null)
+    {
+        if($categorie != null){
+            $manager=$this->getDoctrine()->getManager();
+            $manager->remove($categorie);
+            $manager->flush();
+
+            $this->addFlash("success","Categorie supprimée");
+        }
+        else {
+            $this->addFlash("danger","Categorie introuvable");
+        }
+        return $this->redirectToRoute('/');
+    }
+
+    /**
+     * @Route("/categorie/detail/{id}",name="categorie_detail")
+     */
+    public function detail($id,CategorieRepository $repo){
+
+        $categorie = $repo->find($id);
+
+        return $this->render('categorie/categorie_detail.html.twig',[
+            'categorie' => $categorie,
         ]);
     }
 }
